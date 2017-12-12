265ad228e2b72f5abbb84ca54958f58171fe314d
Liam Barstad <liambarstad@gmail.com>
2017-12-12 14:52:18 -0700

    add stores, roles, userRoleStores

---
+++ db/migrate/20171212215127_create_user_role_stores.rb
@@ -0,0 +1,11 @@
+class CreateUserRoleStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table :user_role_stores do |t|
+      t.references :user, foreign_key: true
+      t.references :role, foreign_key: true
+      t.references :store, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
